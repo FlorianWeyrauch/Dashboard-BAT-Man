83794444637701e8285163e02df243ee37d66cec
FlorianWeyrauch <weyrauch-florian@web.de>
2025-11-24 10:24:33 +0100

    bfw icon

--- overview.php
+++ overview.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kursübersicht</title>
+    <link rel="icon" href="./assets/icon/bfw-icon.svg" type="image/x-icon">
+
+
+    <!-- CSS-Dateien -->
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 <body>
+    <header></header>
+    <main></main>
+    <footer></footer>
     
 </body>
 </html>
