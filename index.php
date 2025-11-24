fcfa5a045964869bf4f08cfef479e023155ffefb
Sven Lehnert <Sven.L@flairgenix.com>
2025-11-24 13:27:18 +0100

    Entferne Hover-Effekte von der Login-Box und füge den Standard-CSS-Link in index.php hinzu

--- index.php
+++ index.php
@@ -9,16 +9,15 @@
     <title>Document</title>
     <link rel="stylesheet" href="./node_modules\bootstrap\dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="./css/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./css/standard.css?v=<?php echo time(); ?>">
     <link rel="icon" href="./assets/icon/bfw-icon.svg" type="image/x-icon">
 </head>
 <body>
-    <header>
-        <div class="header-container container-fluid">
-            <nav class="header align-middle">
-                <img src="./assets/icon/bfw-icon.svg" alt="BFW Icon" class="navbar-brand-icon">
-                <a class="navbar-brand align-middle" href=""><b>bfw</b>nürnberg</a>
-            </nav>
-        </div>
+    <header class="d-flex-sb-c">
+        <a href="./overview.php">
+            <img class="bfw-icon" src="./assets/icon/bfw-icon.svg" alt="BFW">
+        </a>
+        <h1>Willkommen</h1>
     </header>
     <main>
         <div class="main-container">
