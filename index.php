0687e3312ea445dbe3e539588a188fe9ef37bc99
Sven Lehnert <Sven.L@flairgenix.com>
2025-11-24 10:24:37 +0100

    updaste main.css with CSS-Variablen

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    phpinfo();
+    //phpinfo();
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -8,8 +8,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./node_modules\bootstrap\dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    
+    <header>
+        <nav class="navbar navbar-expand-lg header ">
+            <a class="navbar-brand" href="">Dashboard - BAT Man</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </header>
+    <div class="container">
+        <h1 class="text-center">Dashboard</h1>
+    </div>
 </body>
 </html>
