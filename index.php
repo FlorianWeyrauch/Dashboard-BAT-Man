74c1f95f1e77bff3107f3b52c5bc3c64b1958c8b
Sven Lehnert <Sven.L@flairgenix.com>
2025-11-24 10:35:44 +0100

    Aktualisiere CSS-Variablen und verbessere das Header-Layout mit dem BFW-Icon

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
     <title>Document</title>
     <link rel="stylesheet" href="./node_modules\bootstrap\dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="./css/main.css?v=<?php echo time(); ?>">
+    <link rel="icon" href="./assets/icon/bfw-icon.svg" type="image/x-icon">
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg header ">
-            <a class="navbar-brand" href="">Dashboard - BAT Man</a>
+            <img src="./assets/icon/bfw-icon.svg" alt="BFW Icon" class="navbar-brand-icon">
+            <a class="navbar-brand bold" href="">Dashboard - BAT Man</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
